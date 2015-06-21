d3e53b5d35cc0e2698732eb538bb51b51e22f3ad
Matt Farguson <xlsxgeek@gmail.com>
2015-06-21 01:17:13 -0700

    refactor

--- EdgarParser.cs
+++ EdgarParser.cs
@@ -12,46 +12,79 @@ namespace EdgarCrawler
 {
     class EdgarParser
     {
-        public string edgarHtml { get; set; }
+        
         public string DownloadLocation { get; set; }
-        public EdgarParser(string webUrl)
+        public bool IsSuccessful { get; set; }
+        private bool containsForms;
+        private string formDString;
+        private string formDAString;
+        private string edgarHtml;
+        private string pattern;
+
+        public EdgarParser()
         {
-            bool isDownloaded = GetHTML(webUrl);
-            if (isDownloaded)
+            containsForms = true;
+            IsSuccessful = true;
+            DownloadLocation = ConfigurationManager.ConnectionStrings["DownloadLocation"].ConnectionString;
+            formDString = ConfigurationManager.ConnectionStrings["formDHtml"].ConnectionString;
+            formDAString = ConfigurationManager.ConnectionStrings["formDaHtml"].ConnectionString;
+            pattern = @"\d{6,7}\/\d{18}\/";
+            EdgerCycle();
+        }
+
+        private void EdgerCycle()
+        {
+            int interval = 100;
+            int webPageStartNumber = 0;
+            while (containsForms)
             {
-                DownloadLocation = ConfigurationManager.ConnectionStrings["DownloadLocation"].ConnectionString;
-                string formDString = ConfigurationManager.ConnectionStrings["formDHtml"].ConnectionString;
-                cycleThroughString(formDString);
-                string formDAString = ConfigurationManager.ConnectionStrings["formDaHtml"].ConnectionString;
-                cycleThroughString(formDAString);
+                string edgarUrl = ConfigurationManager.ConnectionStrings["EdgarSite"].ConnectionString +
+                                                                  webPageStartNumber.ToString() + "&count=" + interval.ToString();
+
+                bool isDownloaded = getHtml(edgarUrl);
+                if (isDownloaded)
+                {
+
+                    Regex myRegex = new Regex(pattern);
+                    Match myMatch = myRegex.Match(edgarHtml);
+                    if (myMatch.Success)
+                    {
+                        cycleThroughString(formDString);
+                        cycleThroughString(formDAString);
+                    }
+                    else
+                    {
+                        containsForms = false;
+                    }
+                }
+                webPageStartNumber += interval;
             }
         }
 
         private void cycleThroughString(string formString)
         {
-            string myUrl = edgarHtml;
+            string myUrl = edgarHtml;            
             int startPosition = 0;
             while (true)
             {               
                 startPosition = TextUtils.Search(myUrl, formString);
                 if (startPosition > 0)
                 {
-                    myUrl = myUrl.Substring(startPosition + formString.Length);
-                    string pattern = @"\d{6,7}\/\d{18}\/";
+                    myUrl = myUrl.Substring(startPosition + formString.Length);                  
                     Regex myRegex = new Regex(pattern);
                     Match myMatch = myRegex.Match(myUrl);
                     string xmlUrl = "https://www.sec.gov/Archives/edgar/data/" + myMatch.Value + "primary_doc.xml";
                     string fileName = myMatch.Value.Replace("/", "") + ".xml";
-                    Download(xmlUrl, fileName);
+                    download(xmlUrl, fileName);
                 }
                 else
                 {
                     break;
-                }
-            
+                }            
             }
         }
-        private void Download(string xmlUrl, string fileName)
+
+        private void download(string xmlUrl, string fileName)
         {
             if (!File.Exists(DownloadLocation + fileName))
             {
@@ -65,6 +98,7 @@ namespace EdgarCrawler
                 catch(Exception x)
                 {
                     Console.WriteLine(x.Message);
+                    IsSuccessful = false;
                 }                    
             }
             else
@@ -72,7 +106,8 @@ namespace EdgarCrawler
                 Console.WriteLine("File already exists: " + fileName);
             }
         }       
-        private bool GetHTML(string url)
+
+        private bool getHtml(string url)
         {
             bool isDownloaded = false;
             using (WebClient client = new WebClient())
@@ -87,9 +122,11 @@ namespace EdgarCrawler
                 {
                     Console.WriteLine("Problem downloading: "  + x.Message);
                     isDownloaded = false;
+                    IsSuccessful = false;
                 }                
             }
             return isDownloaded;          
-        }   
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -13,8 +13,7 @@ namespace EdgarCrawler
         {
             Console.WriteLine("I grab code from edgar's site and find Form Ds and form D/As");
             Console.WriteLine("I then pull xmls and load them into a file for further execution");
-            string webUrl = ConfigurationManager.ConnectionStrings["EdgarSite"].ConnectionString;
-            var p = new EdgarParser(webUrl);
+            var p = new EdgarParser();
         }
     }
 
