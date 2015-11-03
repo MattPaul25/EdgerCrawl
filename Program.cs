bf7bf37f42653164af42d3d11c2fc453b1bf28a9
unknown <fargusonm@SFO10AC26L1W7.win.dowjones.net>
2015-11-03 12:01:06 -0500

    updated prep

--- Program.cs
+++ Program.cs
@@ -14,12 +14,20 @@ namespace EdgarCrawler
             TextUtils.Comment("I grab code from edgar's site and find Form Ds and form D/As");
             TextUtils.Comment("I then pull xmls and load them into a file for further execution");
 
-            var prep = new Prepare();            
-            TextUtils.Comment("preperation was successful");
-            var p = new EdgarParser();               
-            var ex = new ExcelInterop();
-            var ac = new AccessInterop();
-            var prep2 = new Prepare();
+            var prep = new Prepare();
+            if (prep.IsSuccessful)
+            {
+                TextUtils.Comment("preperation was successful");
+                var p = new EdgarParser();
+                var ex = new ExcelInterop();
+                var ac = new AccessInterop();
+                var prep2 = new Prepare();
+            }
+            else
+            {
+                TextUtils.Comment("Was unable to prepare files.");
+            }
+
         }
 
     }
